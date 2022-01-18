a304361c0b3a2ebd50c4d698dab3690cf8f680fa
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2022-01-18 13:32:32 -0500

    fix babel config

---
+++ .storybook/main.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  stories: [
+    "../src/Affix/stories.@(js|mdx|ts|tsx)",
+    //  "../src/AppBar/stories.@(js|mdx|ts|tsx)",
+    "../src/ClipboardCopyField/stories.@(js|mdx|ts|tsx)",
+
+    //    "../src/Button/stories.@(js|mdx|ts|tsx)",
+  ],
+  babel: async (options) => ({
+    ...options,
+    presets: options.presets.concat([
+      [
+        "@emotion/babel-preset-css-prop",
+        {
+          autoLabel: true,
+          labelFormat: "Uikit-[local]",
+        },
+      ],
+    ]),
+  }),
+  webpackFinal: async (config, { configType }) => {
+    config.node = {
+      __dirname: true,
+      __filename: true,
+    };
+
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve(__dirname, "../"),
+    ];
+
+    return config;
+  },
+};
