9bca7c318b4f3efcb98f512d14fd3e051f0e777d
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2022-03-21 12:34:21 -0400

    setup test. remove old test commands

---
+++ jest-setup.ts
@@ -0,0 +1,3 @@
+import '@testing-library/jest-dom';
+import { matchers } from '@emotion/jest';
+expect.extend(matchers);
---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  // Automatically clear mock calls, instances and results before every test
+  clearMocks: true,
+
+  // A preset that is used as a base for Jest's configuration
+
+  // The test environment that will be used for testing
+  testEnvironment: 'jsdom',
+
+  // The glob patterns Jest uses to detect test files
+  // testMatch: [
+  //   "**/__tests__/**/*.[jt]s?(x)",
+  //   "**/?(*.)+(spec|test).[tj]s?(x)"
+  // ],
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+};
