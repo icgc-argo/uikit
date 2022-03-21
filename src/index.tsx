5f72517471c0ab6e0ce0b21aae14f072fdf70b1d
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2022-03-21 14:25:14 -0400

    export theme as object

--- src/index.tsx
+++ src/index.tsx
@@ -20,6 +20,7 @@
 import defaultTheme from './theme/defaultTheme';
 
 export type UikitTheme = typeof defaultTheme;
+export { defaultTheme };
 export { default as ThemeProvider } from './ThemeProvider';
 export { default as styled } from '@emotion/styled';
 export { default as css } from '@emotion/css';
