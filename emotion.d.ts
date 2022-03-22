b635a54f417ab900d000e7c02afb91fd65970fff
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2022-03-22 00:42:09 -0400

    fix emotion typings

--- emotion.d.ts
+++ emotion.d.ts
@@ -1,6 +1,5 @@
 import '@emotion/react';
 import defaultTheme from './src/theme/defaultTheme';
-type d = typeof defaultTheme;
 declare module '@emotion/react' {
-  export interface Theme extends d {}
+  export interface Theme {}
 }
