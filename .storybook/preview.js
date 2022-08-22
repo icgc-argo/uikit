8914dfe6fd091f194a74e5227a0e7756e350745f
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2022-08-22 14:37:02 -0400

    cleanup

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,3 @@
-// export const parameters = {
-//   actions: { argTypesRegex: '^on[A-Z].*' },
-//   controls: {
-//     matchers: {
-//       color: /(background|color)$/i,
-//       date: /Date$/,
-//     },
-//   },
-// };
-
 import { ThemeProvider } from '../src/index';
 
 export const decorators = [
