8afc6e761dbb8242890f10a4b028527b42061303
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2023-06-12 20:32:01 -0400

    remove border on AppMenuItem

--- src/AppBar/styledComponents.tsx
+++ src/AppBar/styledComponents.tsx
@@ -31,6 +31,7 @@ export const MenuItemContent = styled(MenuItemTypography)`
   text-align: center;
   text-decoration: none;
 `;
+
 export const MenuItemContainer = styled('div', {
   shouldForwardProp: (propName) => propName !== 'active',
 })<{
@@ -51,10 +52,8 @@ export const MenuItemContainer = styled('div', {
   }
 
   color: ${({ active, theme }) => (active ? theme.colors.accent1 : theme.colors.white)};
-  border-left: solid 0.5px ${({ theme }) => theme.colors.grey};
-  border-right: solid 0.5px ${({ theme }) => theme.colors.grey};
-  border-bottom: solid 3px ${({ active, theme }) => (active ? theme.colors.accent1 : 'none')};
 `;
+
 export const MenuGroupDisplay = styled('div')`
   display: flex;
   flex-direction: row;
