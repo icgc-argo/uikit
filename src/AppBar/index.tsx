dea110e757ffea072440b86cd5a205e18c8c3493
Ciarán Schütte <ciaran.schutte@oicr.on.ca>
2023-06-13 13:26:55 -0400

    AppMenuBar and AppMenuItem tweaks (#128)
    * use defaultTheme, allow theme overriding from consumer project
    
    * remove font import in middle of theme provider, add readme note to import in your consumer project
    
    * export default theme as object. can extend in consumer project
    
    * minor upgrade emotion and add to peer deps
    
    * fix packagelock
    
    * uikit happy
    
    * cleanup
    
    * readme updates
    
    * add icon for external entities
    
    * add DataCallout component
    
    * cleanup DataCallout
    
    * data callout styling
    
    * export Emotion global provider
    
    * change semantic element, container can have more things than nav
    
    * remove DomComponent prop, not in use
    
    * remove border on AppMenuItem
    
    * remove dropdown component of AppMenuItem
    
    * AppBar style changes
    
    ---------
    
    Co-authored-by: Ciaran Schutte <ciaranschutte@oicr.on.ca>

--- src/AppBar/index.tsx
+++ src/AppBar/index.tsx
@@ -41,7 +41,7 @@ import {
 export const UserBadge = ({
   firstName = '',
   lastName = '',
-  title = null,
+  title = '',
   showGreeting = true,
   ...otherProps
 }) => {
@@ -120,39 +120,17 @@ export const AppBarMenuItem = React.forwardRef<
     active?: boolean;
     id?: string;
     className?: string;
-    DomComponent?: React.ComponentType;
-    dropdownMenu?: React.ReactNode;
     children?: React.ReactNode;
   }
->(
-  (
-    {
-      children,
-      className,
-      id,
-      active = false,
-      DomComponent = ({ active, ...others }: { active: boolean }) => <a {...others} />,
-      dropdownMenu,
-    },
-    forwardedRef,
-  ) => {
-    const ref = (forwardedRef || React.createRef()) as React.RefObject<HTMLDivElement>;
-    const [isDropdownOpen, setDropdownOpen] = React.useState(false);
+>(({ children, className, id, active = false }, forwardedRef) => {
+  const ref = (forwardedRef || React.createRef()) as React.RefObject<HTMLDivElement>;
 
-    useClickAway({
-      domElementRef: ref,
-      onClickAway: () => setDropdownOpen(false),
-      onElementClick: () => setDropdownOpen(!isDropdownOpen),
-    });
-
-    return (
-      <MenuItemContainer className={className} id={id} ref={ref} active={active}>
-        <MenuItemContent bold>{children}</MenuItemContent>
-        {isDropdownOpen && dropdownMenu}
-      </MenuItemContainer>
-    );
-  },
-);
+  return (
+    <MenuItemContainer className={className} id={id} ref={ref} active={active}>
+      <MenuItemContent bold>{children}</MenuItemContent>
+    </MenuItemContainer>
+  );
+});
 
 export const AppBar = AppBarContainer;
 
